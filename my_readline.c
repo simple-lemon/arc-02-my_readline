f49a74c0489bdc8b8e1bac7ec9e1b38d7879733a
Khalid <akbar2002kenjayev@gmail.com>
2023-12-07 12:08:36 +0500

    Finished

--- my_readline.c
+++ my_readline.c
@@ -118,25 +118,3 @@ char *my_readline(int fd){
         tmp = my_strcat(tmp, FULL_CONTENT);
     }
 }
-
-
-int main(int ac, char av)
-{
-char *str = NULL;
-
-
-int fd = open("./file.txt", O_RDONLY);
-while ((str = my_readline(fd)) != NULL)
-{
-printf("%s\n", str);
-free(str);
-}
-close(fd);
-//
-//  Yes it's also working with stdin :-)
-//  printf("%s", my_readline(0));
-//
-
-
-return 0;
-}
