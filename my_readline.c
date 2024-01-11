df0fcb7215ccfea3ae4677b240cf3bde1d755df1
xadicha <246abdurashidova123@gmail.com>
2024-01-12 00:29:29 +0500

    my_readline.c file created

---
+++ my_readline.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int READLINE_READ_SIZE = 512;
+
+char *my_readline(int fd);
+
+void init_my_readline();
+
+int main(int ac, char **av)
+{
+  char *str = NULL;
+
+  int fd = open("./file.txt", O_RDONLY);
+  while ((str = my_readline(fd)) != NULL)
+  {
+      printf("%s\n", str);
+      free(str);
+  }
+  close(fd);
+  //
+  //  Yes it's also working with stdin :-)
+  //  printf("%s", my_readline(0));
+  //
+
+  return 0;
+}
