40fa70c2a79fcdebf36e23c961084a86d537f4ea
Nurulloh <140059040+abdurashidovnurulloh@users.noreply.github.com>
2024-02-28 15:32:36 +0500

    Update my_readline.c

--- my_readline.c
+++ my_readline.c
@@ -1,135 +1,150 @@
-#include <fcntl.h>
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h> 
+#include <unistd.h>
+#include <fcntl.h>
 
-short READLINE_READ_SIZE= 20;
-char* store = NULL;
+int READLINE_READ_SIZE = 6;
+char* store;
 
-void null(char* str){
-    for(int i=0; i<(int)strlen(str); i++){
-        str[i]='\0';
-    }
-}
-
-void fill_null(char* str, int ind){
-    for(int i=ind; i<(int)strlen(str); i++){
-        str[i]='\0';
-    }
-}
-
-char* finish(char* temp, char* storage, int ind){
-    char* res = (char*)calloc(strlen(temp)+ind+1,sizeof(char));
-    int i=0;
-    while(i<(int)strlen(temp)){
-        res[i]=temp[i];
-        i++;
-    }
-    for(int j=0; j<ind; j++){
-        res[i]=storage[j];
-        i++;
-    }
-    if (!storage[ind+1] || !(storage+ind+1)){
-        *storage='\0';
-    }
-    else{
-        char* new = (char*)calloc((int)strlen(storage)+1,sizeof(char));
-        strcpy(new,&storage[ind+1]);
-        strcpy(storage, new);
-        free(new);
-    }
-    free(temp);
-    return res;
-}
-
-char* mine_strcat(char* temp, char* storage){
-    char* result = (char*)calloc(strlen(temp)+strlen(storage)+1, sizeof(char));
-    int ind=0;
-    while(ind<(int)strlen(temp)){
-        result[ind]=temp[ind];
-        ind++;
-    }
-
-    for (int j=0; j<(int)strlen(storage); j++){
-        result[ind]=storage[j];
-        ind++;
-    }
-    free(temp);
-    return result;
-}
-
-void init_my_readline(){
-    if(store != NULL) {
-     free(store);
-    }
-}
-
-char *my_readline(int fd){
-    if (!store){
-        int s;
-        store=(char*)calloc(READLINE_READ_SIZE+1, sizeof(char));
-
-        s = read(fd, store, READLINE_READ_SIZE);
-        if(!s){
-            init_my_readline();
-            return NULL;
-        }
-    }
-
-    for (int i=0; i<(int)strlen(store); i++){
-        if (store[i]=='\n'){
-            char* result = (char*)calloc(i+1,sizeof(char));
-            strncpy(result, store, i);
-
-            char* new = (char*)calloc((int)strlen(store)+1,sizeof(char));
-            strcpy(new, &store[i+1]);
-            strcpy(store, new);
-            free(new);
-            return result;
-        }
-    }
-    
-    char* tmp = (char*)calloc((int)strlen(store)+1,sizeof(char));
-    strcpy(tmp, store);
-    int s;
-    while(1){
-        null(store);
-        s = read(fd, store, READLINE_READ_SIZE);
-
-        if(s) fill_null(store, s);
-        
-        if (!s){
-            if(!*tmp){
-                free(tmp);
-                init_my_readline();
-                return NULL;
-            }
-            store[0]='\0';
-            return tmp;    
-        }
-        for (int i=0; i<(int)strlen(store); i++){
-            if (store[i]=='\n'){
-                return finish(tmp, store, i);
-            }
-        }
-        tmp = mine_strcat(tmp, store);
-    }
-}
-
-int main() 
+typedef struct listnode
 {
-    init_my_readline();
-    char *str = NULL;
-    // int fd = open("./file.txt", O_RDONLY);
+    int size;
+    int count;
+    char* text;
+} list;
 
-    // while ((str = my_readline(fd))) 
-    // {
-    //     printf("%s\n", str);
-    //     free(str);
-    // }
+char* my_strcpy(char* str1, char* str2) {
+    for(int i = 0; str2[i]; i++) {
+        str1[i] = str2[i];
+    }
+    return str1;
+}
+
+int my_strlen(char* string)
+{
+    int i = 0;
+    while(string[i] != '\0')
+        i++;
+    return i;
+}
+
+void fill_null(char* string, int length)
+{
+    int i = 0;
+    while(i <= length)
+    {
+        string[i] = '\0';
+        i++;
+    }
+}
+
+char* my_realloc(char* content, int over)
+{
+    char* temp = malloc(sizeof(char) * over + 1 + READLINE_READ_SIZE);
+    fill_null(temp, over + READLINE_READ_SIZE);
+    my_strcpy(temp, content);
+    free(content);
+    return temp;
+}
+
+void copy_store(char* after_new_line)
+{
+    fill_null(store, READLINE_READ_SIZE);
+    short i = 0;
+    for( ; after_new_line[i]; i++) {
+        store[i] = after_new_line[i];
+    }
+    store[i] = '\0';
+}
+
+void init_my_readline()
+{
+    if(store) {
+        free(store);
+    }
+    store = malloc(sizeof(char) * READLINE_READ_SIZE + 1);
+    fill_null(store, READLINE_READ_SIZE);
+}
+
+int check_line(list* answer, char* temp)
+{   
+    int length = my_strlen(temp);
+    int i = 0;
+    while(i < length)
+    {
+        if(temp[i] == '\n')
+        {
+            copy_store(&temp[i + 1]);
+            return 1;
+        }
+
+        if(answer->size == (READLINE_READ_SIZE) * answer->count)
+        {
+            answer->count++;
+            answer->text = my_realloc(answer->text, answer->size);
+        }
+        answer->text[answer->size++] = temp[i];
+        i++;
+    }
+    return 0;
+}
+
+char* my_readline(int fd)
+{
+    if(fd != 3 && fd != 0){
+        return NULL;
+    }
+    list answer;
+    answer.count = 1;
+    answer.size = 0;
+    answer.text = malloc(sizeof(char) * (READLINE_READ_SIZE + 1));
+    fill_null(answer.text, READLINE_READ_SIZE);
+    char* temp = NULL;
+    if(store)
+    {
+        temp = malloc(sizeof(char) * (READLINE_READ_SIZE + 1));
+        fill_null(temp, READLINE_READ_SIZE);
+        my_strcpy(temp, store);
+
+        if(check_line(&answer, temp) == 1)
+        {
+            free(temp);
+            return answer.text;
+        }
+    }
+    fill_null(temp, READLINE_READ_SIZE);
+    while(read(fd, temp, READLINE_READ_SIZE))
+    {
+        if(check_line(&answer, temp) == 1)
+        {
+            free(temp);
+            return answer.text;
+        }
+       fill_null(temp, READLINE_READ_SIZE);
+    }
+    if(answer.text[0]){
+        free(temp);
+        return answer.text;
+    }
+    free(temp);
+    free(answer.text);
+    return NULL;
+}
+
+int main() {
     
-    // close(fd);
+    char *str = NULL;
+    init_my_readline();
+
+    int fd = open("./file.txt", O_RDONLY);
+
+    while ((str = my_readline(fd)) != NULL) {
+        printf("%s\n", str);
+        free(str);
+    }
+    close(fd);
+    
     str = my_readline(0);
     printf("%s\n", str);
     free(str);
